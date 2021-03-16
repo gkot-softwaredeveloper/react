9bf72ec597ced0d6b4b7eb8a329fe347c5e86a6a
Grzegorz Kot <gkot.softwaredeveloper@gmail.com>
2021-03-16 22:44:20 +0100

    Improve tests and ci/cd  test added

--- fundamentals/src/App.test.tsx
+++ fundamentals/src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Render two types of component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const classComponentItem = screen.getByText(/Hello from Header as a Class Component/i);
+  const functionalComponentItem = screen.getByText(/Hello from Header as a Function Component/i);
+  expect(classComponentItem).toBeInTheDocument();
+  expect(functionalComponentItem).toBeInTheDocument();
 });
