949f4fc2a1ef6671b94253abb38ce886c63e69fc
Grzegorz Kot <gkot.softwaredeveloper@gmail.com>
2021-03-23 22:41:37 +0100

    Added a little bit code regarding component life cycle in class components, added react-faker package to generating random data

--- fundamentals/src/App.tsx
+++ fundamentals/src/App.tsx
@@ -4,8 +4,13 @@ import LifeCycle from "./components/component_lifecycle/LifeCycle";
 import { LifeCycleOld } from "./components/component_lifecycle/LifeCycleOld";
 import Render from "./components/condition_rendering/Render";
 import HeaderFunctional from "./components/functional/HeaderFunctional";
+import { useState } from "react";
+import { useFaker } from 'react-fakers'
 
 function App() {
+  const[oldLifeCycleTitle, setOldLifeCycleTitle] = useState<string>('');
+  const {success} = useFaker({});
+ 
   return (
     <>
       <div className="App">
@@ -14,7 +19,10 @@ function App() {
       </div>
       <Render dataTestId="render" />
       <LifeCycle dataTestId="app-lifecycle"/>
-      <LifeCycleOld dataTestId="app-lifecycleold" title="Hello from Old Life Cycle" />
+      <LifeCycleOld dataTestId="app-lifecycleold" title={oldLifeCycleTitle} />
+      <input type="button" onClick={()=>{
+        setOldLifeCycleTitle(success[0].firstname);
+      }} value="Get Title"/>
     </>
   );
 }
--- fundamentals/src/components/component_lifecycle/LifeCycleOld.tsx
+++ fundamentals/src/components/component_lifecycle/LifeCycleOld.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 
 interface LifeCycleOldProps {
-    title: string;
-    dataTestId: string;
+  title: string;
+  dataTestId: string;
 }
+
 export class LifeCycleOld extends React.Component<LifeCycleOldProps> {
-    constructor(props: LifeCycleOldProps) {
-        super(props);
-    }
-    render(){
-        return (<div data-testid={this.props.dataTestId}>{this.props.title}</div>)
-    }
+  constructor(props: LifeCycleOldProps) {
+    super(props);
+    console.log("This message came from constructor");
+  }
+  static RunBeforeRender(): void {
+    console.log("This message came from static function");
+  }
+  componentDidMount(): void {
+    console.log("This message came from componentDidMount function");
+  }
+  componentDidUpdate(): void {
+    console.log("This message came from componentDidUpdate function");
+  }
+
+  render() {
+    console.log("This message came from render function");
+    return <div data-testid={this.props.dataTestId}>{this.props.title}</div>;
+  }
 }
